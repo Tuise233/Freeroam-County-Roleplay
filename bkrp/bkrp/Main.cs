376c179ca6794c1289a0cd0d4be110c5de2c0a18
Tuise233 <2039203690@qq.com>
2022-04-21 03:05:01 +0800

    新增主程序入口

---
+++ bkrp/bkrp/Main.cs
@@ -0,0 +1,36 @@
+﻿using AltV.Net;
+using AltV.Net.Data;
+using AltV.Net.Elements.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace bkrp
+{
+    class Main : IScript
+    {
+        public Main()
+        {
+            Alt.OnAnyResourceStart += Alt_OnAnyResourceStart;
+            Alt.OnPlayerConnect += Alt_OnPlayerConnect;
+            Alt.OnPlayerDisconnect += Alt_OnPlayerDisconnect;
+        }
+
+        private void Alt_OnAnyResourceStart(INativeResource resource)
+        {
+            Log.Server("Altv - Freeroam County Role Play - 2022 启动成功");
+            Log.Server("Coded By Brandon_Karl");
+        }
+
+        private void Alt_OnPlayerDisconnect(IPlayer player, string reason)
+        {
+            EventManager.Call_OnPlayerDisconnected(player, reason);
+        }
+
+        private void Alt_OnPlayerConnect(IPlayer player, string reason)
+        {
+            EventManager.Call_OnPlayerConnected(player);
+        }
+    }
+}
