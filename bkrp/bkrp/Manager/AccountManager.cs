7bf85fd9980ff09c6acf3fca8a146bcc80ac9ac0
Tuise233 <2039203690@qq.com>
2022-04-21 00:40:21 +0800

    实现帐号登录、注册功能

--- bkrp/bkrp/Manager/AccountManager.cs
+++ bkrp/bkrp/Manager/AccountManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AltV.Net;
+using AltV.Net.Elements.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,86 @@ using System.Threading.Tasks;
 
 namespace bkrp
 {
-    class AccountManager
+    class AccountManager : IScript
     {
+        public AccountManager()
+        {
+            EventManager.OnPlayerConnected += EventManager_OnPlayerConnected;
+        }
+
+        private void EventManager_OnPlayerConnected(IPlayer player)
+        {
+            if(!player.HasMetaData("login"))
+            {
+                player.Emit("account:load");
+            }
+        }
+
+        [ClientEvent("RegisterAccount")]
+        public void RegisterAccount(IPlayer player, string username, string password, string email)
+        {
+            Log.Server($"注册帐号: {username} {password} {email}");
+            Database.ExecuteSql($"SELECT * FROM `account` WHERE `username`='{username}'", (reader, err) =>
+            {
+                if(err)
+                {
+                    player.Emit("accont:client-displayErr", "数据库异常抛出");
+                    return;
+                }
+                else
+                {
+                    while(reader.Read())
+                    {
+                        player.Emit("account:client-displayErr", "您输入的用户名已被注册");
+                        return;
+                    }
+
+                    //注册账号
+                    Database.ExecuteSql($"INSERT INTO `account` (`username`, `password`, `email`) VALUES ('{username}', '{password}', '{email}')", (err) =>
+                        {
+                            if (err)
+                            {
+                                player.Emit("accont:client-displayErr", "数据库异常抛出");
+                                return;
+                            }
+                            else
+                            {
+                                //注册成功
+                                Log.Server("注册成功");
+                            }
+                        });
+                }
+            });
+        }
+
+        [ClientEvent("LoginAccount")]
+        public void LoginAccount(IPlayer player, string username, string password)
+        {
+            Log.Server($"登录账号: {username} {password}");
+            Database.ExecuteSql($"SELECT * FROM `account` WHERE `username`='{username}'", (reader, err) =>
+            {
+                if(err)
+                {
+                    player.Emit("accont:client-displayErr", "数据库异常抛出");
+                    return;
+                }
+                else
+                {
+                    while(reader.Read())
+                    {
+                        if(reader.GetString("password") == password)
+                        {
+                            Log.Server("登陆成功");
+                            return;
+                        }
+                        else
+                        {
+                            player.Emit("account:client-displayErr", "您输入的账号或密码有误");
+                            return;
+                        }
+                    }
+                }
+            });
+        }
     }
 }
---
+++ resources/gamemodepackage/webview/account/index.js
@@ -0,0 +1,66 @@
+/// <reference types="@altv/types-client" />
+/// <reference types="@altv/types-natives" />
+import * as alt from 'alt-client';
+import * as native from 'natives';
+
+let accountView = null;
+
+alt.onServer('account:load', () => {
+    alt.showCursor(true);
+    accountView = new alt.WebView('http://resource/webview/account/index.html');
+    accountView.isVisible = false;
+    accountView.focus();
+
+    accountView.on('accont:client-Login', (name, password) => {
+        console.log(name, password);
+
+        if(!name || !password){
+            return displayErr('您的用户名或密码未填写');
+        }
+
+        if(name.length < 5 || name.length > 15){
+            return displayErr('您的用户名长度不在5~15个单位内');
+        }
+
+        if(password.length < 5 || password.length > 15){
+            return displayErr('您的密码长度不在5~15个单位内');
+        }
+
+        alt.emitServer('LoginAccount', name, password);
+    });
+
+    accountView.on('account:client-Register', (name, password, email) => {
+        console.log(name, password, email);
+
+        if(!name || !password){
+            return displayErr('您的用户名或密码未填写');
+        }
+
+        if(name.length < 5 || name.length > 15){
+            return displayErr('您的用户名长度不在5~15个单位内');
+        }
+
+        if(password.length < 5 || password.length > 15){
+            return displayErr('您的密码长度不在5~15个单位内');
+        }
+
+        if(!email || email.indexOf('@') === -1){
+            return displayErr('您的邮箱无效');
+        }
+
+        alt.emitServer('RegisterAccount', name, password, email);
+    });
+});
+
+
+alt.onServer('account:client-displayErr', displayErr);
+function displayErr(type){
+    accountView.emit('account:view-displayErrorMsg', type);
+}
+
+
+alt.onServer('account:destroy', () => {
+    accountView.unfocus();
+    accountView.destroy();
+    alt.showCursor(false);
+});
