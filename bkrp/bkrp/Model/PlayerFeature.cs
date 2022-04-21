3f1145f81606bf6a808c02ec462fc89ebd340195
Tuise233 <2039203690@qq.com>
2022-04-21 10:36:18 +0800

    新增PlayerFeature模型，新增CharacterCreator

---
+++ bkrp/bkrp/Model/PlayerFeature.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bkrp
+{
+    public class PlayerFeature
+    {
+        public int Gender;
+        public ParentData Parents;
+        public ClothesData Clothes = new ClothesData();
+        public AccessoryData Accessory = new AccessoryData();
+        public float[] Features = new float[20];
+        public AppearanceItem[] Appearance = new AppearanceItem[13];
+        public HairData Hair;
+
+        public int EyebrowColor;
+        public int BeardColor;
+        public int EyeColor;
+        public int BlushColor;
+        public int LipstickColor;
+        public int MakeUp;
+        public int ChestHairColor;
+
+        public PedTattoos PedTatttoos;
+
+
+        public PlayerFeature()
+        {
+            Gender = 0;
+            Parents = new ParentData(0, 0, 1.0f, 1.0f);
+            for (int i = 0; i < Features.Length; i++) Features[i] = 0f;
+            for (int i = 0; i < Appearance.Length; i++) Appearance[i] = new AppearanceItem(255, 1.0f, 0);
+            Hair = new HairData(0, 0, 0);
+        }
+    }
+
+    public class ParentData
+    {
+        public int Father;
+        public int Mother;
+        public float Similarity;
+        public float SkinSimilarity;
+
+        public ParentData(int father, int mother, float sim, float skinsim)
+        {
+            this.Father = father;
+            this.Mother = mother;
+            this.Similarity = sim;
+            this.SkinSimilarity = skinsim;
+        }
+    }
+
+    public class ComponentItem
+    {
+        public int Variation;
+        public int Texture;
+
+        public ComponentItem(int variation, int texture)
+        {
+            Variation = variation;
+            Texture = texture;
+        }
+    }
+
+    public class ClothesData
+    {
+        public ComponentItem Mask { get; set; }
+        public ComponentItem Gloves { get; set; }
+        public ComponentItem Torso { get; set; }
+        public ComponentItem Leg { get; set; }
+        public ComponentItem Bag { get; set; }
+        public ComponentItem Feet { get; set; }
+        public ComponentItem Accessory { get; set; }
+        public ComponentItem Undershit { get; set; }
+        public ComponentItem Bodyarmor { get; set; }
+        public ComponentItem Decals { get; set; }
+        public ComponentItem Top { get; set; }
+
+        public ClothesData()
+        {
+            Mask = new ComponentItem(0, 0);
+            Gloves = new ComponentItem(0, 0);
+            Torso = new ComponentItem(15, 0);
+            Leg = new ComponentItem(21, 0);
+            Bag = new ComponentItem(0, 0);
+            Feet = new ComponentItem(34, 0);
+            Accessory = new ComponentItem(0, 0);
+            Undershit = new ComponentItem(15, 0);
+            Bodyarmor = new ComponentItem(0, 0);
+            Decals = new ComponentItem(0, 0);
+            Top = new ComponentItem(15, 0);
+        }
+    }
+
+    public class AccessoryData
+    {
+        public ComponentItem Hat { get; set; }
+        public ComponentItem Glasses { get; set; }
+        public ComponentItem Ear { get; set; }
+        public ComponentItem Watches { get; set; }
+        public ComponentItem Bracelets { get; set; }
+
+        public AccessoryData()
+        {
+            Hat = new ComponentItem(-1, 0);
+            Glasses = new ComponentItem(-1, 0);
+            Ear = new ComponentItem(-1, 0);
+            Watches = new ComponentItem(-1, 0);
+            Bracelets = new ComponentItem(-1, 0);
+        }
+    }
+
+    public class AppearanceItem
+    {
+        public int Value;
+        public float Opacity;
+        public int Color;
+
+        public AppearanceItem(int value, float opacity, int color)
+        {
+            Value = value;
+            Opacity = opacity;
+            Color = color;
+        }
+    }
+
+    public class HairData
+    {
+        public int Hair;
+        public int Color;
+        public int HighlightColor;
+
+        public HairData(int hair, int color, int highlightcolor)
+        {
+            Hair = hair;
+            Color = color;
+            HighlightColor = highlightcolor;
+        }
+    }
+
+    public struct PedTattoos
+    {
+        public List<KeyValuePair<string, string>> TorsoTattoos;
+        public List<KeyValuePair<string, string>> HeadTattoos;
+        public List<KeyValuePair<string, string>> LeftArmTattoos;
+        public List<KeyValuePair<string, string>> RightArmTattoos;
+        public List<KeyValuePair<string, string>> LeftLegTattoos;
+        public List<KeyValuePair<string, string>> RightLegTattoos;
+        public List<KeyValuePair<string, string>> BadgeTattoos;
+    }
+}
---
+++ bkrp/bkrp/Tool/System/JsonConvers.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace bkrp
+{
+    public interface IJsonParser
+    {
+        System.Type ParserType { get; }
+        string ConvertoJson(object obj);
+    }
+
+    public static class JsonConvers
+    {
+        public static Dictionary<System.Type, IJsonParser> Parser = new Dictionary<System.Type, IJsonParser>();
+
+        /// <summary>
+        /// 设置解析器
+        /// </summary>
+        public static void SetJsonParser(IJsonParser jsonParser)
+        {
+            Parser[jsonParser.ParserType] = jsonParser;
+        }
+
+        /// <summary>
+        /// 嵌套对象,entityName
+        /// </summary>
+        /// <param name="lists"></param>
+        /// <param name="entityName"></param>
+        /// <returns></returns>
+        public static string ConvertJson(List<object> lists, string entityName)
+        {
+            try
+            {
+                StringBuilder result = new StringBuilder();
+                //当传进来的entityName=Emtpty的时候，随机获取lists[index]项的类型
+                if (entityName.Equals(string.Empty))
+                {
+                    object obj = lists[0];
+                    entityName = obj.GetType().ToString();
+                }
+                result.Append($"{{\"{entityName}\":");
+                result.Append(ListToJson(lists));
+                result.Append('}');
+                return result.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+                return "Conver Error";
+            }
+        }
+
+
+
+        public static string ToJson(this object obj)
+        {
+            int deep = 0;
+            return ObjectToJson(obj, ref deep);
+        }
+        public static string ObjectToJson(object item, ref int deep)
+        {
+            try
+            {
+                if (item == null)
+                {
+                    return "null";
+                }
+                var tt = item.GetType();
+                if (Parser.ContainsKey(tt))
+                {
+                    try
+                    {
+                        return Parser[tt].ConvertoJson(item);
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Server($"JSON解析器 [{tt.Name}] 无法正常解析:{ex.Message}");
+                    }
+                }
+                if (tt == typeof(string))//字符串
+                {
+                    return $"\"{item.ToString()}\"";
+                }
+                if (tt == typeof(bool))//布尔值
+                {
+                    return ((bool)item) ? "true" : "false";
+                }
+                if (!tt.IsClass)//不是类类型
+                {
+                    return item.ToString();
+                }
+                else //是类类型对象
+                {
+                    //是容器  普通类类型数组解析
+                    if (item is IList<object>)
+                    {
+                        return ListToJson((item as IList<object>));
+                    }
+                    else
+                    if (item is IDictionary<object, object>)
+                    {
+                        return DictionaryToJson((item as IDictionary<object, object>));
+                    }
+                    else
+                    if (item is IEnumerable<object>)
+                    {
+                        return EnumerableToJson((item as IEnumerable<object>));
+                    }
+                    else //增加Int类型的解析
+                    if (item is IList<int>)
+                    {
+                        return ListToJson((item as IList<int>));
+                    }
+                    else
+                    if (item is IDictionary<int, object>)
+                    {
+                        return DictionaryToJson((item as IDictionary<int, object>));
+                    }
+                    else
+                    if (item is IEnumerable<int>)
+                    {
+                        return EnumerableToJson((item as IEnumerable<int>));
+                    }
+                    else
+                    {
+                        try
+                        {
+                            //是对象
+                            return ClassJson(item, ref deep);
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Error("不能转换此种类型至JSON, 请补全解析检测, 或使用装箱模式 在JsonConvers.cs 102" + item.GetType().Name);
+                            Log.Error(ex.Message);
+                            return item.ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Server("ObjectToJson Error" + ex.Message);
+                return "";
+            }
+        }
+        public static string ListToJson<T>(IList<T> lists)
+        {
+            try
+            {
+                StringBuilder result = new StringBuilder();
+                result.Append('[');
+                foreach (var item in lists)
+                {
+                    int deep = 0;
+                    result.Append(ObjectToJson(item, ref deep) + ",");
+                }
+                if (result[result.Length - 1] == ',')
+                    result.Remove(result.Length - 1, 1);
+                result.Append(']');
+                return result.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                Log.Server("ListToJson Error: " + ex.Message);
+                return "";
+            }
+        }
+        public static string DictionaryToJson<T, T2>(IDictionary<T, T2> lists)
+        {
+            try
+            {
+                StringBuilder result = new StringBuilder("{");
+                foreach (var item in lists)
+                {
+                    int deep = 0;
+                    result.Append($"\"{item.Key.ToString()}\":{ObjectToJson(item.Value, ref deep)},");
+                }
+                if (result[result.Length - 1] == ',')
+                    result.Remove(result.Length - 1, 1);
+                result.Append('}');
+                return result.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                Log.Server("DictionaryToJson Error: " + ex.Message);
+                return "";
+            }
+        }
+        public static string EnumerableToJson<T>(IEnumerable<T> lists)
+        {
+            try
+            {
+                StringBuilder result = new StringBuilder();
+                result.Append('[');
+                foreach (var item in lists)
+                {
+                    int deep = 0;
+                    result.Append(ObjectToJson(item, ref deep) + ",");
+                }
+                if (result[result.Length - 1] == ',')
+                    result.Remove(result.Length - 1, 1);
+                result.Append(']');
+                return result.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                Log.Server("EnumerableToJson Error: " + ex.Message);
+                return "";
+            }
+        }
+        public static string ClassJson(object item, ref int deep)
+        {
+            try
+            {
+                deep += 1;
+                if (deep > 10)
+                {
+                    Log.Error("克隆深度超过极限");
+                    return "Too Deep!";
+                }
+                StringBuilder result = new StringBuilder("{");
+                PropertyInfo[] parms = item.GetType().GetProperties();
+                foreach (var p in parms)//获取所有对象
+                {
+                    if (p.GetCustomAttribute(typeof(JsonConverIgnoreAttribute)) != null) continue;
+                    var value = p.GetValue(item);
+                    result.Append("\"" + p.Name.ToString() + "\":" + ObjectToJson(value, ref deep) + ',');
+                }
+                FieldInfo[] parmsf = item.GetType().GetFields();
+                foreach (var p in parmsf)
+                {
+                    if (p.GetCustomAttribute(typeof(JsonConverIgnoreAttribute)) != null) continue;
+                    var value = p.GetValue(item);
+                    result.Append("\"" + p.Name.ToString() + "\":" + ObjectToJson(value, ref deep) + ',');
+                }
+                if (result[result.Length - 1] == ',')
+                    result.Remove(result.Length - 1, 1);
+                result.Append('}');
+                return result.ToString();
+            }
+            catch (Exception ex)
+            {
+                Log.Server("ListToJson Error" + ex.Message);
+                return "";
+            }
+        }
+    }
+
+    public class JsonConverIgnoreAttribute : Attribute
+    {
+        public JsonConverIgnoreAttribute()
+        {
+
+        }
+
+    }
+}
