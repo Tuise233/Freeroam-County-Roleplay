f9c82493134554bd2e0feb67366d9b134956fdb1
Tuise233 <2039203690@qq.com>
2022-04-21 10:37:01 +0800

    新增client-camera系统

---
+++ resources/gamemodepackage/tool/camera/index.js
@@ -0,0 +1,23 @@
+/// <reference types="@altv/types-client" />
+/// <reference types="@altv/types-natives" />
+import * as alt from 'alt-client';
+import * as native from 'natives';
+
+let camera = null;
+
+alt.on("camera:create", createCamera);
+alt.on("camera:destroy", destroyCamera);
+
+function createCamera(x, y, z, rx, ry, rz, fov){
+    if(camera == null){
+        camera = native.createCamWithParams("DEFAULT_SCRIPTED_CAMERA", x, y, z, rx, ry, rz, fov, true, 2);
+        native.renderScriptCams(true, false, 0, true, true, 1);
+    }
+}
+
+function destroyCamera(){
+    if(camera != null){
+        native.destroyCam(camera);
+        camera = null;
+    }
+}
