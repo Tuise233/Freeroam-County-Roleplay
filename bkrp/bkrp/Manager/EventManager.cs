522d2471c1ca0cc3b4142db90eabbf5dc837bac8
Tuise233 <2039203690@qq.com>
2022-04-21 10:35:49 +0800

    优化EventManager，对接PlayerEx

--- bkrp/bkrp/Manager/EventManager.cs
+++ bkrp/bkrp/Manager/EventManager.cs
@@ -12,15 +12,22 @@ namespace bkrp
         /// <summary>
         /// 当玩家连接时
         /// </summary>
-        public static event Action<IPlayer> OnPlayerConnected = null;
+        public static event Action<PlayerEx> OnPlayerConnected = null;
 
         /// <summary>
         /// 当玩家断开连接
         /// </summary>
-        public static event Action<IPlayer, string> OnPlayerDisconnected = null;
+        public static event Action<PlayerEx, string> OnPlayerDisconnected = null;
+
+        /// <summary>
+        /// 当玩家结束登录系统
+        /// </summary>
+        /// <param name="player"></param>
+        public static event Action<PlayerEx, AccountModel> OnPlayerLogin = null;
 
         //调用
-        public static void Call_OnPlayerConnected(IPlayer player) => OnPlayerConnected?.Invoke(player);
-        public static void Call_OnPlayerDisconnected(IPlayer player, string reason) => OnPlayerDisconnected?.Invoke(player, reason);
+        public static void Call_OnPlayerConnected(IPlayer player) => OnPlayerConnected?.Invoke(player as PlayerEx);
+        public static void Call_OnPlayerDisconnected(PlayerEx player, string reason) => OnPlayerDisconnected?.Invoke(player, reason);
+        public static void Call_OnPlayerLogin(PlayerEx player, AccountModel model) => OnPlayerLogin?.Invoke(player, model);
     }
 }
