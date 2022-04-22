5a78a6f0f8ad9d61da349ae254fa67f5e200b5ca
Tuise233 <2039203690@qq.com>
2022-04-22 09:18:30 +0800

    新增PlayerCharacter类

---
+++ bkrp/bkrp/Model/PlayerCharacter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bkrp
+{
+    public class PlayerCharacter
+    {
+        public int id { get; set; }
+        public int uid { get; set; }
+        public string name { get; set; }
+        public int money { get; set; }
+        public int bank { get; set; }
+        public int age { get; set; }
+        public int level { get; set; }
+        public int exp { get; set; }
+
+        public PlayerCharacter()
+        {
+            this.uid = -1;
+            this.name = "";
+            this.money = 0;
+            this.bank = 0;
+            this.age = 0;
+            this.level = 0;
+            this.exp = 0;
+        }
+    }
+}
