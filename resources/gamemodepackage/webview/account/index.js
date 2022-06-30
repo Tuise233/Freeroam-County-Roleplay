151c289047efa9ff56e325513c96f8b98f79f991
Tuise233 <2039203690@qq.com>
2022-06-30 21:26:38 +0800

    重写账号界面

--- resources/gamemodepackage/webview/account/index.js
+++ resources/gamemodepackage/webview/account/index.js
@@ -5,66 +5,61 @@ import * as native from 'natives';
 
 let accountView = null;
 
-alt.onServer('account:load', () => {
+alt.onServer("account:load", () => {
     if(accountView == null){
         alt.showCursor(true);
-        accountView = new alt.WebView('http://resource/webview/account/index.html');
+        accountView = new alt.WebView("http://resource/webview/account/index.html");
         accountView.isVisible = true;
         accountView.focus();
 
-        accountView.on('accont:client-Login', (name, password) => {
-            console.log(name, password);
-
+        accountView.on("account:client-Login", (name, password) => {
             if(!name || !password){
-                return displayErr('您的用户名或密码未填写');
+                return displayErr("您的用户名或密码未填写");
             }
 
-            if(name.length < 5 || name.length > 15){
-                return displayErr('您的用户名长度不在5~15个单位内');
+            if(name.length < 5 || name.length > 18){
+                return displayErr("用户名长度规定在5~18个单位");
             }
 
-            if(password.length < 5 || password.length > 15){
-                return displayErr('您的密码长度不在5~15个单位内');
+            if(password.length < 5 || password.length > 18){
+                return displayErr("密码长度规定在5~18个单位");
             }
 
-            alt.emitServer('LoginAccount', name, password);
+            alt.emitServer("LoginAccount", name, password);
         });
 
-        accountView.on('account:client-Register', (name, password, email) => {
-            console.log(name, password, email);
-
+        accountView.on("account:client-Register", (name, password, email) => {
             if(!name || !password){
-                return displayErr('您的用户名或密码未填写');
+                return displayErr("您的用户名或密码未填写");
             }
 
-            if(name.length < 5 || name.length > 15){
-                return displayErr('您的用户名长度不在5~15个单位内');
+            if(name.length < 5 || name.length > 18){
+                return displayErr("用户名长度规定在5~18个单位");
             }
 
-            if(password.length < 5 || password.length > 15){
-                return displayErr('您的密码长度不在5~15个单位内');
+            if(password.length < 5 || password.length > 18){
+                return displayErr("密码长度规定在5~18个单位");
             }
 
-            if(!email || email.indexOf('@') === -1){
-                return displayErr('您的邮箱无效');
+            if(!email || !email.includes("@")){
+                return displayErr("您输入的邮箱无效");
             }
 
-            alt.emitServer('RegisterAccount', name, password, email);
+            alt.emitServer("RegisterAccount", name, password, email);
         });
     }
 });
 
-
-alt.onServer('account:client-displayErr', displayErr);
-function displayErr(type){
-    accountView.emit('account:view-displayErrorMsg', type);
-}
-
-
-alt.onServer('account:destroy', () => {
+alt.onServer("account:destroy", () => {
     if(accountView != null){
         accountView.unfocus();
         accountView.destroy();
+        accountView = null;
         alt.showCursor(false);
     }
 });
+
+alt.onServer("account:client-displayErr", displayErr);
+function displayErr(type){
+    accountView.emit("account:view-displayErrorMsg", type);
+}
